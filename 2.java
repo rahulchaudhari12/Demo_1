dc5b75823c75c73056920a12bee72c35701bb039
rahulchaudhari12 <146310273+rahulchaudhari12@users.noreply.github.com>
2023-09-28 09:44:07 +0530

    Add files via upload

---
+++ 2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Sqaure 
+{
+public static void main(String [] args)
+
+ {
+Scanner in = new Scanner(System.in); 
+
+int n,s;
+
+System.out.println("Enter the number ");
+
+n= in.nextInt();
+
+s=n*n;
+
+System.out.println("Sqaure = "+s);
+
+}
+
+}
